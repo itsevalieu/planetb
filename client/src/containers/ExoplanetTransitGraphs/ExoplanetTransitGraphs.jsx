3906a0b73ec1acd8da7c0e1b5795d248f959f333
itsevalieu <itsevalieu@gmail.com>
2019-04-21 16:49:26 -0700

    Make React components for all views

---
+++ client/src/containers/ExoplanetTransitGraphs/ExoplanetTransitGraphs.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExoplanetTransitGraph extends Component {
+  render() {
+    return (
+      <div>
+        Exoplanet Transit Graph
+      </div>
+    );
+  }
+}
+
+export default ExoplanetTransitGraph;
---
+++ client/src/containers/TransitObservations/CommunityTransitObservationsList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CommunityTransitObservationsList extends Component {
+  render() {
+    return (
+      <div>
+        Community Transit Observations List
+      </div>
+    );
+  }
+}
+
+export default CommunityTransitObservationsList;
