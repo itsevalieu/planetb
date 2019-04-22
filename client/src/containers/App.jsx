5059feab67c50a8f4955e5faeff2d599b7f39eed
itsevalieu <itsevalieu@gmail.com>
2019-04-21 22:15:40 -0700

    Setting up react router for ExoplanetList, TransitObservations, and ExoplanetTransitGraphs

---
+++ client/src/containers/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom';
+import './App.css';
+import ExoplanetList from './ExoplanetList/ExoplanetList';
+import ExoplanetTransitGraphs from './ExoplanetTransitGraphs/ExoplanetTransitGraphs';
+import TransitObservations from './TransitObservations/TransitObservations';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header><a href='/'>Planet B</a></header>
+        <Router>
+          <Route exact path='/' component={ ExoplanetList } />
+          <Route exact path='/exoplanet' component={ ExoplanetTransitGraphs } />
+          <Route exact path='/exoplanet/transit' component={ TransitObservations } />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
