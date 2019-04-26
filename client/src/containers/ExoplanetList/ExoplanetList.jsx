2532891df399127e83349bc325ed3caf11c366bb
Michelle Li <michelle.li.862@gmail.com>
2019-04-25 17:18:19 -0700

    issue32: Connected stitch to retrieve list of exoplanets

--- client/src/containers/ExoplanetList/ExoplanetList.jsx
+++ client/src/containers/ExoplanetList/ExoplanetList.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import Async from 'react-promise';
+
+const {
+  Stitch,
+  RemoteMongoClient,
+  AnonymousCredential
+} = require('mongodb-stitch-browser-sdk');
+const client = Stitch.initializeDefaultAppClient('planetb-kopdp');
+const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('planetb');
 
 class ExoplanetList extends Component {
+  getExoplanets(){
+    return new Promise(function(resolve, reject){
+        client.auth.loginWithCredential(new AnonymousCredential()).then(() =>
+        db.collection('exoplanets').find({}, { limit: 10}).asArray()
+      ).then(docs => {
+        console.log(docs);
+        console.log("[MongoDB Stitch] Connected to Stitch");
+        resolve(docs);
+      }).catch(err => {
+        console.error(err);
+        reject(Error("MongoDB Stitch Error"));
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         ExoplanetList
+        <Async promise={this.getExoplanets()} then={val => val.map(function(d, idx){
+          return (<li key={idx}>{d.object_name}</li>)
+        })}/>
       </div>
     );
   }
