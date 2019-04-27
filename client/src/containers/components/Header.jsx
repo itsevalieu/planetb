3a548657a9e62b598f81ba0fcc9e8b998a8dd5f5
itsevalieu <itsevalieu@gmail.com>
2019-04-27 08:55:43 -0700

    Adding logo

--- client/src/containers/components/Header.jsx
+++ client/src/containers/components/Header.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import Button from './Button';
 import styled from 'styled-components';
+import logo from './planetb.png';
 
-const Nav = styled.nav`
-      font-size: 3em,
-      width: 100%,
-      text-align: center,
-      padding: 1em,
-      color: #fff,
-      text-shadow: 2px 2px 2px #333
+const StyledHeader = styled.header`
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    padding: 1em;
+`;
+const Title = styled.h1`
+    text-shadow: 2px 2px 2px #333
+    font-size: 3em;
+    color: #fff;
+`;
+const Link = styled.a`
+    color: #fff;
 `;
 
 const Header = (props) => {
+    let style = {
+        width: '80px',
+        height: '70px'
+    }
     if (props.children === 'Exoplanet List') {
-        return <Nav><a href='/'>Planet B</a><h1>{props.children}</h1></Nav>;
+        return <StyledHeader><Link href='/'><img src={logo} alt='logo' style={style}/></Link><Title>{props.children}</Title><Link href='/'><Button>Login</Button></Link></StyledHeader>;
     } else if (props.children === 'Exoplanet Transit Graphs') {
-        return <Nav><a href='/'>Planet B</a><h1>{props.children}</h1><Button>Transit Observations</Button></Nav>;    
+        return <StyledHeader><Link href='/'><img src={logo} alt='logo' style={style}/></Link><Title>{props.children}</Title><Link href='/exoplanet/transit'><Button>Transit Observations</Button></Link></StyledHeader>;    
     } else if (props.children === 'Transit Observations') {
-        return <Nav><a href='/'>Planet B</a><h1>{props.children}</h1><Button>Exoplanet Graphs</Button></Nav>;
+        return <StyledHeader><Link href='/'><img src={logo} alt='logo' style={style}/></Link><Title>{props.children}</Title><Link href='/exoplanet'><Button>Exoplanet Graphs</Button></Link></StyledHeader>;
     } else {
         return null;
     }
