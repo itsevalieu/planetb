f528142b0d4bd4bd852d83527cfa490b6d164aa4
itsevalieu <itsevalieu@gmail.com>
2019-04-30 02:44:03 -0700

    Create getTransit function, complete styling for transit items

--- client/src/containers/TransitObservations/TransitObservationItem.jsx
+++ client/src/containers/TransitObservations/TransitObservationItem.jsx
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-class TransitObservationItem extends Component {
-  render() {
-    return (
-      <div>
-        Transit Observation Item
-      </div>
-    );
+const Row = styled.tr`
+  font-size: 16px;
+  &:hover, &:active {
+    background: #ccc;
+    cursor: pointer;
   }
+`;
+const Cell = styled.td`
+  padding: 10px;
+  text-align: center;
+  border-top: 1px solid #ccc;
+`;
+
+const TransitObservationItem = ({index, data}) => {
+  return (
+    <Row>
+      <Cell>{index + 1}</Cell>
+      <Cell>{data.HJD_mid}</Cell>
+      <Cell>{data.epoch}</Cell>
+      <Cell>{data.OC}</Cell>
+      <Cell>{data.duration}</Cell>
+      <Cell>{data.depth}</Cell>
+      <Cell>{data.band}</Cell>
+      <Cell>{data.data_quality}</Cell>
+      <Cell></Cell>
+      <Cell>{data.observer + ' ' + data.reference}</Cell>
+      <Cell>*</Cell>
+      <Cell>{(data.insert_date).toDateString()}</Cell>
+    </Row>
+  );
 }
 
 export default TransitObservationItem;
